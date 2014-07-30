9db108f9ca1377ae6401ab6c645b0498b5a89518
Paula Freitas <paulathefreitas@gmail.com>
2014-07-30 20:53:20 +0100

    Progress of the very first day

---
+++ script.js
@@ -0,0 +1,17 @@
+// This is where all the practise code from the Introduction to Javascript tutorial goes
+
+console.log("hello! this is my first javascript");
+var name = "Codebar ";
+console.log (name + "is amazing"); //no one can see this comment 
+
+var pi = 3.14; 
+console.log("The value of pi: "+pi);
+
+var codebarIsAwesome = true;
+var weatherIsAmazing = false;
+
+console.log("Is Codebar Awesome?" + codebarIsAwesome); 
+console.log("Is the weather in London amazing " + weatherIsAmazing); 
+
+var iDontHaveAValue; 
+console.log("What kind of variable am I? " + iDontHaveAValue);
